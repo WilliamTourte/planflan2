dcdecc25774bbe59b8053ba72d1223d5e00c7f06
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-09-23 15:20:34 +0200

    Affichage des établissements et flans

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
-from app.models import Etablissement
+from app.models import Etablissement, Flan
 
 main_bp = Blueprint('main', __name__)
 
@@ -16,10 +16,21 @@ def dashboard():
 @main_bp.route('/etablissements')
 def afficher_etablissements():
     etablissements = lister_etablissements()
-    return render_template('etablissements.html', etablissements=etablissements)
+    return render_template('liste_etablissements.html', etablissements=etablissements)
 
 def lister_etablissements():
     etablissements = Etablissement.query.all()
-    for etablissement in etablissements:
-        print(f"ID: {etablissement.id_etab}, Nom: {etablissement.nom}, Adresse: {etablissement.adresse}")
     return etablissements
+
+@main_bp.route('/etablissement/<int:id_etab>')   # Capture l'ID dans l'URL
+def afficher_etablissement_unique(id_etab):
+    etab_unique = Etablissement.query.get(id_etab)  # Récupère l'établissement par son ID
+    if etab_unique is None:
+        abort(404)  # Retourne une erreur 404 si l'établissement n'existe pas
+    return render_template('page_etablissement.html', etablissement=etab_unique)  # Passe l'établissement au template
+
+@main_bp.route('/flan/<int:id_flan>')
+def afficher_flan_unique(id_flan):
+    flan = Flan.query.get_or_404(id_flan)
+    return render_template('flan.html', flan=flan)
+
