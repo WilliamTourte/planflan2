e69dd50d6d8b9487d1f0d40688ee6ab0720f2182
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-09-24 21:06:24 +0200

    Nettoyage dans les routes main

--- app/routes/main.py
+++ app/routes/main.py
@@ -16,22 +16,16 @@ def dashboard():
 
 @main_bp.route('/etablissements')
 def afficher_etablissements():
-    etablissements = lister_etablissements()
+    etablissements = Etablissement.query.all() # Prend tous les établissements
     return render_template('liste_etablissements.html', etablissements=etablissements)
-
-def lister_etablissements():
-    etablissements = Etablissement.query.all()
-    return etablissements
 
 @main_bp.route('/etablissement/<int:id_etab>')   # Capture l'ID dans l'URL
 def afficher_etablissement_unique(id_etab):
-    etab_unique = Etablissement.query.get(id_etab)  # Récupère l'établissement par son ID
-    if etab_unique is None:
-        abort(404)  # Retourne une erreur 404 si l'établissement n'existe pas
+    etab_unique = Etablissement.query.get_or_404(id_etab)  # Récupère l'établissement par son ID ou 404
     return render_template('page_etablissement.html', etablissement=etab_unique)  # Passe l'établissement au template
 
 @main_bp.route('/flan/<int:id_flan>')
 def afficher_flan_unique(id_flan):
-    flan = Flan.query.get_or_404(id_flan)
-    return render_template('flan.html', flan=flan)
+    flan_unique = Flan.query.get_or_404(id_flan) # Récupère le flan par son ID ou 404 si l'id n'existe pas
+    return render_template('flan.html', flan=flan_unique)  # Passe le flan au template
 
