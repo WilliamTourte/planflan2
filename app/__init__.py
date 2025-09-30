bf1db8fef479c5e21e77380c16269e44fab15dcf
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-09-30 10:23:25 +0200

    Nettoyage du code

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-from dotenv import load_dotenv
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -12,7 +10,6 @@ db = SQLAlchemy() # Doit être la SEULE instance de SQLAlchemy
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'  # Route pour la page de login
 login_manager.login_message = "Veuillez vous connecter pour accéder à cette page."
-
 bcrypt = Bcrypt()
 
 def create_app():
@@ -21,9 +18,7 @@ def create_app():
     # initialisation des extensions
     db.init_app(app)
     login_manager.init_app(app)
-
-    # Vérifie que la clé est bien chargée
-    print("Clé API Google Maps:", os.getenv('GOOGLE_MAPS_API_KEY'))  # ✅ Debug
+    bcrypt.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -33,8 +28,6 @@ def create_app():
     @app.template_filter('enlever_accents') # filtre Jinja2 pour enlever les accents
     def filtre_enlever_accents(text):
         return enlever_accents(text)
-
-    bcrypt.init_app(app)
 
     # Enregistre les blueprints dans l'application
     from app.routes.auth import auth_bp
