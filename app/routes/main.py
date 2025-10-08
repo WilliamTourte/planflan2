24d49afe9bfc0d0f3570d539710f97ac9088b1d6
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-10-08 10:59:07 +0200

    Ca marche bien avec la possiblité d'ajouter un flan

--- app/routes/main.py
+++ app/routes/main.py
@@ -53,10 +53,6 @@ def dashboard():
     return render_template('dashboard.html', title='Tableau de bord', form=form, pending_evaluations=pending_evaluations)
 
 
-
-from flask import request
-from flask import request
-
 @main_bp.route('/modifier_evaluation/<int:id_eval>', methods=['POST'])
 @login_required
 def modifier_evaluation(id_eval):
@@ -179,7 +175,7 @@ def afficher_etablissement_unique(id_etab):
     form = NewFlanForm()  # Instancie un formulaire si on veut proposer un nouveau flan
     form.id_etab.data = id_etab  # Remplit le champ caché
     if form.validate_on_submit():  # Si le formulaire est soumis et valide
-        # Traiter les données ici
+
         pass
     # Passe toujours le formulaire au template, même en GET
     return render_template('page_etablissement.html', etablissement=etablissement, form=form, current_user=current_user)
@@ -188,8 +184,7 @@ def afficher_etablissement_unique(id_etab):
 def afficher_flan_unique(id_flan):
     flan_unique = Flan.query.get_or_404(id_flan) # Récupère le flan par son ID ou 404 si l'id n'existe pas
     evaluations = flan_unique.evaluations # Pour que les évaluations soient transmises
-    for eval in evaluations:
-        print(f"ID: {eval.id_eval}, Statut: {eval.statut}")
+
     return render_template('page_flan.html', flan=flan_unique, request=request) # request pour se souvenir du endpoint  # Passe le flan au template
 
 @main_bp.route('/etablissement/<int:id_etab>/proposer_flan', methods=['GET', 'POST'])
@@ -220,7 +215,7 @@ def proposer_flan(id_etab):
 def evaluer_flan(id_flan):
     flan_unique = Flan.query.get_or_404(id_flan)
     form = EvalForm()
-    # Vérifiez si une évaluation existe déjà pour cet utilisateur et ce flan
+    # Vérifier si une évaluation existe déjà pour cet utilisateur et ce flan
     evaluation = Evaluation.query.filter_by(id_flan=id_flan, id_user=current_user.id_user).first()
     if form.validate_on_submit():
         moyenne = (
