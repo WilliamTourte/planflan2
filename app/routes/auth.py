31c66d2c1eb214d63f805c21dd7884256838fffd
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-10-13 11:40:41 +0200

    Modifications de l'apparence

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -52,7 +52,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('main.index'))
 
 @auth_bp.route('/supprimer_compte', methods=['POST'])
 @login_required
