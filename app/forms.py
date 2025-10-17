a257b0671a4a7acddef87eb8d2c76517f8ea89df
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-10-17 12:37:59 +0200

    On essaye de repartir sur la base qui marchait bien sans rejoindre routes

--- app/forms.py
+++ app/forms.py
@@ -77,12 +77,20 @@ class EvalForm(FlaskForm):
     submit = SubmitField('Proposer une évaluation')
 
 # Formulaire pour rechercher un établissement
-class ChercheEtabForm(FlaskForm):
+class RechercheForm(FlaskForm):
     nom = StringField('Nom', validators=[Optional(), Length(min=3, max=50)])
     ville = StringField('Ville', validators=[Optional(), Length(min=3, max=50)])
+    type_saveur = SelectField('Saveur', choices=[('tous', 'Tous')] + [(choice.name, choice.value) for choice in TypeSaveur], default='tous')
+    type_pate = SelectField('Pâte', choices=[('tous', 'Tous')] + [(choice.name, choice.value) for choice in TypePate], default='tous')
+    type_texture = SelectField('Texture', choices=[('tous', 'Tous')] + [(choice.name, choice.value) for choice in TypeTexture], default='tous')
+    prix = prix = SelectField('Gamme de prix', choices=[('tous', 'Tous'), (0, "Moins de 2€50"), (2.5, "Plus de 2€50 mais moins de 5€"), (5, "Plus de 5€")], default='tous')
+
+    submit = SubmitField('Rechercher')
+    # SEULEMENT POUR ADMIN #
     visite = RadioField('Visité', choices=[('tous', 'Tous'), ('oui', 'Oui'), ('non', 'Non')], default='tous')
     labellise = RadioField('Labellisé', choices=[('tous', 'Tous'), ('oui', 'Oui'), ('non', 'Non')], default='tous')
-    submit = SubmitField('Rechercher')
+
+
 
 
 
