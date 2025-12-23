8327393254bed927e20d623a522cbd6742148495
WiliamTourte <damien.hugonnard@gmail.com>
2025-12-23 18:54:58 +0100

    calculer distance dans outils

--- app/outils.py
+++ app/outils.py
@@ -1,5 +1,5 @@
 from flask import url_for
-
+from math import radians, sin, cos, sqrt, atan2
 
 def enlever_accents(text): # Enlève les accents parce que la police ne les gère pas bien
     import unicodedata
@@ -18,15 +18,15 @@ def afficher_etablissements(resultats):
 
 from math import radians, sin, cos, sqrt, atan2
 
-def calculer_distance(lat1, lon1, lat2, lon2):
-    # Rayon de la Terre en kilomètres
-    R = 6371.0
 
+
+def calculer_distance(lat1, lon1, lat2, lon2):
+    R = 6371.0
+    # Convertir toutes les valeurs en float avant de les convertir en radians
+    lat1, lon1, lat2, lon2 = map(float, [lat1, lon1, lat2, lon2])
     lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
     dlat = lat2 - lat1
     dlon = lon2 - lon1
-
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
     return R * c
