95a50f4bbc07a70be9ea293c3167c9bb352bc021
WiliamTourte <damien.hugonnard@gmail.com>
2025-12-28 10:38:47 +0100

    Base de tests créée

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+# tests/test_app.py
+from app import create_app, db
+import pytest
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+        yield client
+
+
+def test_example(client):
+    response = client.get('/')
+    assert response.status_code == 200
