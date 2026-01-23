bcc8305656ba6de51f9fd72c4f61e42dce541452
WiliamTourte <damien.hugonnard@gmail.com>
2026-01-23 21:04:17 +0100

    WIP de + en + de tests passent

--- tests/javascript/setupTests.js
+++ tests/javascript/setupTests.js
@@ -39,42 +39,16 @@ global.fetchMock = fetchMock;
 
 // Activer fetch-mock avant les tests
 beforeAll(() => {
-  fetchMock.config.overwriteRoutes = false;
+  if (fetchMock.config) {
+    fetchMock.config.overwriteRoutes = false;
+  }
 });
 
 // Nettoyer après les tests
 afterAll(() => {
-  fetchMock.restore();
-});
-
-// Mock des fonctions globales qui pourraient être utilisées
-global.showToast = jest.fn();
-global.showLoading = jest.fn();
-global.hideLoading = jest.fn();
-
-// Configuration pour les tests avec DOM
-beforeEach(() => {
-  // Configurer jsdom avec les propriétés nécessaires
-  Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: jest.fn(),
-      removeListener: jest.fn(),
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
-  });
-  
-  // Mock pour la géolocalisation
-  global.navigator.geolocation = {
-    getCurrentPosition: jest.fn(),
-    watchPosition: jest.fn(),
-    clearWatch: jest.fn(),
-  };
+  if (typeof fetchMock.restore === 'function') {
+    fetchMock.restore();
+  }
 });
 
 // Mock pour les modules ES6 qui pourraient être importés
@@ -84,4 +58,28 @@ jest.mock('fetch-mock', () => {
     ...originalModule,
     default: originalModule,
   };
+});
+
+// Configuration supplémentaire pour les tests
+global.console = {
+  log: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+};
+
+// Mock pour HTMLFormElement.prototype.submit qui n'est pas implémenté dans jsdom
+beforeEach(() => {
+  if (typeof HTMLFormElement !== 'undefined') {
+    HTMLFormElement.prototype.submit = jest.fn(function() {
+      // Simuler la soumission du formulaire
+      const event = new Event('submit', { cancelable: true });
+      this.dispatchEvent(event);
+      if (!event.defaultPrevented) {
+        // Si le formulaire n'est pas annulé, on peut simuler la navigation
+        // ou juste logger pour les tests
+        console.log('Form submitted:', this.action, this.method);
+      }
+    });
+  }
 });
