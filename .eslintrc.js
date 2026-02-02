cc168fe1d97475b999a0c9b9fdb3fa53423172db
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2026-02-02 10:14:58 +0100

    Cohérence linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,29 +18,48 @@ module.exports = {
     "rules": {
         "jsdoc/check-alignment": "error",
         "jsdoc/check-indentation": "error",
-        "jsdoc/require-description": "error",
-        "jsdoc/require-param": "error",
-        "jsdoc/require-returns": "error",
+        "jsdoc/require-description": "warn",
+        "jsdoc/require-param": "warn",
+        "jsdoc/require-returns": "warn",
         "jsdoc/require-jsdoc": [
-            "error",
+            "warn",
             {
                 "require": {
                     "FunctionDeclaration": true,
                     "MethodDefinition": true,
                     "ClassDeclaration": true,
-                    "ArrowFunctionExpression": false
-                }
+                    "ArrowFunctionExpression": false,
+                    "FunctionExpression": false
+                },
+                "contexts": [
+                    "any-function"
+                ]
             }
         ],
         "jest/no-disabled-tests": "warn",
         "jest/no-focused-tests": "error",
         "jest/no-identical-title": "error",
         "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error"
+        "jest/valid-expect": "error",
+        "max-len": [
+            "warn",
+            {
+                "code": 100,
+                "ignorePattern": "(^\\s*import |^\\s*export )",
+                "ignoreStrings": true,
+                "ignoreTemplateLiterals": true
+            }
+        ]
     },
     "overrides": [
         {
-            "files": ["tests/javascript/**/*.test.js"],
+            "files": ["tests/**/*.test.js", "tests/**/*.spec.js"],
+            "rules": {
+                "jsdoc/require-jsdoc": "off"
+            }
+        },
+        {
+            "files": ["*.config.js"],
             "rules": {
                 "jsdoc/require-jsdoc": "off"
             }
