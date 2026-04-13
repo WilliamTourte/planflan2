57890e16e98e98052956d26c847aac1aa7eda128
WiliamTourte <damien.hugonnard@gmail.com>
2026-04-13 18:05:17 +0200

    Travail certificat

--- letsencrypt/renew-certificates.sh
+++ letsencrypt/renew-certificates.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Renouveler les certificats
-docker compose run --rm certbot renew
+# Arrêter nginx temporairement
+docker compose stop nginx
 
-# Recharger la configuration nginx pour appliquer les nouveaux certificats
-docker compose exec nginx nginx -s reload
+# Attendre que nginx soit complètement arrêté
+sleep 10
+
+# Renouveler les certificats en utilisant le serveur intégré de certbot
+# Utiliser --standalone pour que certbot gère lui-même le serveur de challenge
+docker compose run --rm --service-ports certbot renew --standalone --force-renewal
+
+# Redémarrer nginx avec les nouveaux certificats
+docker compose up -d nginx
 
 echo "Renouvellement des certificats terminé."
